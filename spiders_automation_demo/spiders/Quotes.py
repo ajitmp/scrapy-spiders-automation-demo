7213bf795b9773ab9e13f882f78fe530e32420b6
Ajit Kumar <ajit.megaproject@gmail.com>
2024-07-27 19:24:47 +0900

    added comment about html tags for each selectors

--- spiders_automation_demo/spiders/Quotes.py
+++ spiders_automation_demo/spiders/Quotes.py
@@ -7,9 +7,12 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com/"]
 
     def parse(self, response):
+        #each quote is within <div class="quote" ...>
         quotes = response.css("div.quote")
         for quote in quotes:
+            #each quote text is within <span class="text" ...>
             title=quote.css("span.text::text").get()
+            #each author info is within <small class="author" ...>
             author =quote.css("small.author::text").get()
             yield{
                 'title':title,
