c3bd30595d031d025926264bd661c2705c53fd0a
Ajit Kumar <ajit.megaproject@gmail.com>
2024-07-27 20:13:13 +0900

    enabled item in the spider QuoteItem

--- spiders_automation_demo/items.py
+++ spiders_automation_demo/items.py
@@ -1,12 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
+#spiders_automation_demo/items.py
 import scrapy
 
+class QuoteItem(scrapy.Item):
+    title = scrapy.Field()
+    author = scrapy.Field()
 
-class SpidersAutomationDemoItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+
--- spiders_automation_demo/spiders/Quotes.py
+++ spiders_automation_demo/spiders/Quotes.py
@@ -1,4 +1,5 @@
 import scrapy
+from spiders_automation_demo.items import QuoteItem
 
 
 class QuotesSpider(scrapy.Spider):
@@ -10,11 +11,16 @@ class QuotesSpider(scrapy.Spider):
         #each quote is within <div class="quote" ...>
         quotes = response.css("div.quote")
         for quote in quotes:
+            #create your item object
+            quote_item = QuoteItem()
             #each quote text is within <span class="text" ...>
             title=quote.css("span.text::text").get()
             #each author info is within <small class="author" ...>
             author =quote.css("small.author::text").get()
-            yield{
-                'title':title,
-                'author':author
-            }
+
+            #add your selector to your item
+
+            quote_item['title']=title
+            quote_item['author']= author
+            #yield your item
+            yield quote_item
