05c05bfce1fd207a3722156764d5e5580eb57ff1
Ajit Kumar <ajit.megaproject@gmail.com>
2024-07-27 23:33:34 +0900

    added and enabled pipeline to filter quote having either love or life in the title text

--- spiders_automation_demo/pipelines.py
+++ spiders_automation_demo/pipelines.py
@@ -1,13 +1,10 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-
-class SpidersAutomationDemoPipeline:
+class FilterQuotesPipelineLoveOrLife:
     def process_item(self, item, spider):
-        return item
+        # Check if 'love' or 'life' is in the text
+        if 'love' in item['title'].lower() or 'life' in item['title'].lower():
+            return item
+        else:
+            raise DropItem(f"Quote without 'love' or 'life' in title text: {item['title']}")
+
